22c8de091333b43c6de71496944bcc3819b59b46
Yuhan Si <yuhansi@usc.edu>
2016-08-28 15:33:07 -0700

    deleted extra layout files

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -2,12 +2,36 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_colors);
+        setContentView(R.layout.word_list);
+
+        // Create a list of words
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("red", "weṭeṭṭi"));
+        words.add(new Word("mustard yellow", "chiwiiṭә"));
+        words.add(new Word("dusty yellow", "ṭopiisә"));
+        words.add(new Word("green", "chokokki"));
+        words.add(new Word("brown", "ṭakaakki"));
+        words.add(new Word("gray", "ṭopoppi"));
+        words.add(new Word("black", "kululli"));
+        words.add(new Word("white", "kelelli"));
+
+        // Create a WordAdapter whose data source is a list of Words
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        // Find the ListView object in the view hierarchy of the ColorsActivity
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // Make the ListView by using the WordAdapter, so that the ListView will display list item for each Word in the list
+        listView.setAdapter(adapter);
+
     }
 }
--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -2,12 +2,38 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family);
+        setContentView(R.layout.word_list);
+
+        // Create a list of words
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("father", "әpә"));
+        words.add(new Word("mother", "әṭa"));
+        words.add(new Word("son", "angsi"));
+        words.add(new Word("daughter", "tune"));
+        words.add(new Word("older brother", "taachi"));
+        words.add(new Word("younger brother", "chalitti"));
+        words.add(new Word("older sister", "teṭe"));
+        words.add(new Word("younger sister", "kolliti"));
+        words.add(new Word("grandmother ", "ama"));
+        words.add(new Word("grandfather", "paapa"));
+
+        // Create a WordAdapter whose data source is a list of Words
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        // Find the ListView object in the view hierarchy of the FamilyActivity
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // Make the ListView by using the WordAdapter, so that the ListView will display list item for each Word in the list
+        listView.setAdapter(adapter);
+
     }
 }
